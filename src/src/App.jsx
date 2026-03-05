d721fe8a5f242a59a02ae7cbbbd1eca5338d46ae
debarshi-DX <debarshi@demandlane.com>
2026-03-05 21:48:43 +0530

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{padding:40,fontFamily:"sans-serif"}}>
+      <h1>LeadFlow Dashboard</h1>
+      <p>If you can see this, deployment worked 🎉</p>
+    </div>
+  );
+}
